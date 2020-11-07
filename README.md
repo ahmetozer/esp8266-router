022742ec9bb37cacb05940eb01f10507825e5d4c
Ahmet OZER <git@ahmetozer.org>
2020-11-07 05:02:38 +0300

    nat enabled

--- README.md
+++ README.md
@@ -1,39 +1,41 @@
 # Extend Wifi Range With ESP8266 (NodeMCU)
+
 This project goal is extending WiFi range with ESP8266.
   Currently It is only supports IPv4 .
 
-# Configuration
+## Configuration
+
 This system is requires Two way Configuration.
 One of them is your router other one is ESP8266 (NodeMCU).
 
-## ESP8266 Side
+### ESP8266 Side
 
 - Set your Home router WiFi SSID and password
+
 ```c++
-const char* ssid     = "Dijitaller.com";
-const char* password = "MyhomeNetworkPass";
+const char* SSID     = "ahmetozer.org";
+const char* PASSWORD = "MyhomeNetworkPass";
 ```
+
 - Set your ESP Wifi name, password and channel
+
 ```c++
-const char* ssid_soft_ap     = "ahmetozer.org2";
-const char* password_soft_ap = password;
+const char* SOFT_AP_SSID     = "ahmet.engineer";
+const char* SOFT_AP_PASSWORD = password;
 int Wifi_SoftAP_Channel      = 11;
 ```
 
 - Set your sub local network on ESP8266
 .**Note:** Each ESP use /29 block but requires /24 block.
+
 ```c++
-IPAddress local_IP(10,0,1,1);
-IPAddress gateway(10,0,1,1);
-IPAddress subnet(255,255,255,0);
+IPAddress SOFT_AP_Local_IP(10,0,1,1);
+IPAddress SOFT_AP_Gateway(10,0,1,1);
+IPAddress SOFT_AP_Subnet(255,255,255,0);
 ```
 
-- Set Your ESP8266 hostname.
-```c++
-const char* espHostname =     "WifiReperater2";
-```
+### Home Side
 
-## Home Side
 1. Firstly compile and flash firmware to your NodeMCU.  
 2. Bootup your ESP8266.  
 3. Find ESP8266 IPAddress on your router.  
@@ -42,12 +44,16 @@ const char* espHostname =     "WifiReperater2";
 
 <img src="https://github.com/ahmetozer/esp8266-router/blob/master/advanced_router_settings.jpg?raw=true" alt="Advanced route setting for ESP8266" onerror="this.src='advanced_router_settings.jpg';" />
 
+## Performance Tunings
 
-
-# Performance Tunings
 - Change channel 9,10 or 11 on your Router.
 - Select Same Wifi channel on your ESP8266.
 it is delegated with `Wifi_SoftAP_Channel` variable.
 - Change lwIP library version to "V2 IPv6 High Bandwidth" or "V2 High Bandwidth".
+
+## Without Configuring Home Side - Update
+
+Nat is Disabled by default for better performance but if you don't have chance to edit route table on router or having issue, you can enable NAT on ESP8266.
+**NOTE: You have to select lwIP library version to "V2 High Bandwidth" for enabling nat. Other wise nat will not work.
 
 ## [Source Code - Github](https://github.com/ahmetozer/esp8266-router) | [BLOG](https://ahmetozer.org/ESP8266-Wifi-Range-Extender.html)
--- esp8266-router.ino
+++ esp8266-router.ino
@@ -1,32 +1,127 @@
 #include <ESP8266WiFi.h>
 
-// Write Your Router SSID which is your wifi name and your wifi password.
-const char* ssid     = "Dijitaller.com";
-const char* password = "MyhomeNetworkPass";
+#include <lwip/dns.h>
+#include <dhcpserver.h>
+
+#if LWIP_FEATURES
+
+#define SERIAL_DEBUG true // To show details on serial
+// Write Your Router SSID which is your wifi name and your wifi PASSWORD.
+const char *SSID = "ahmetozer.org";
+const char *PASSWORD = "12345678";
 
 // Your ESP Wifi
-const char* ssid_soft_ap     = "ahmetozer.org2";
-const char* password_soft_ap = password;          // My preference is same as my home ssid password.
-int Wifi_SoftAP_Channel      = 11;
+const char *SOFT_AP_SSID = "ahmet.engineer";
+const char *SOFT_AP_PASSWORD = PASSWORD; // My preference is same as my home SSID PASSWORD.
+int SOFT_AP_Channel = 11;
 
 // ESP 8266 Local Network Configuration
-IPAddress local_IP(10,0,1,1);
-IPAddress gateway(10,0,1,1);
-IPAddress subnet(255,255,255,0);
+IPAddress SOFT_AP_Local_IP(10, 0, 1, 1);
+IPAddress SOFT_AP_Gateway(10, 0, 1, 1);
+IPAddress SOFT_AP_Subnet(255, 255, 255, 0);
+#define Copy_DNS_Settings true // if this is set to true
+// System copy connected modem dns settings to dns server, other wise set to Cloudflare Public DNS
+IPAddress SOFT_AP_DNS1(1, 1, 1, 1);
+IPAddress SOFT_AP_DNS2(1, 0, 0, 1);
 
 // Rename Your ESP name to find easily on your router
-const char* espHostname =     "WifiReperater2";
-   
-void setup() {
+const char *espHostname = SOFT_AP_SSID; // By default, hostname is AP SSID but if you want you can set by manual by replace SOFT_AP_SSID with "YourHostName";
+
+#define NAT_Enabled false
+
+#if NAT_Enabled  && !LWIP_IPV6
+#define NAPT 1000
+#define NAPT_PORT 10
+#include <lwip/napt.h>
+#endif
+
+void setup()
+{
+#if SERIAL_DEBUG
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("\n\n\nStarting ...");
+#endif
   WiFi.mode(WIFI_AP_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    WiFi.hostname(espHostname);
+#if SERIAL_DEBUG
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
+#endif
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+#if SERIAL_DEBUG
+    Serial.print(".");
+#endif
     delay(500);
   }
-  WiFi.softAPConfig(local_IP, gateway, subnet);
-  WiFi.softAP(ssid_soft_ap, password_soft_ap, Wifi_SoftAP_Channel);
+  WiFi.hostname(espHostname);
+#if Copy_DNS_Settings
+  SOFT_AP_DNS1 = WiFi.dnsIP(0);
+  SOFT_AP_DNS2 = WiFi.dnsIP(1);
+#endif
+
+  dhcps_set_dns(0, SOFT_AP_DNS1);
+  dhcps_set_dns(1, SOFT_AP_DNS2);
+
+  WiFi.softAPConfig(SOFT_AP_Local_IP, SOFT_AP_Gateway, SOFT_AP_Subnet);
+  WiFi.softAP(SOFT_AP_SSID, SOFT_AP_PASSWORD, SOFT_AP_Channel);
+#if SERIAL_DEBUG
+  // Wifi Client Part
+  Serial.printf("\nConnected WiFi %s\n MAC addr: %s\nESP8266 client ip address: %s \nDNS adresses: %s , %s\n",
+  SSID, WiFi.softAPmacAddress().c_str(),
+  WiFi.localIP().toString().c_str(),
+  WiFi.dnsIP(0).toString().c_str(),
+  WiFi.dnsIP(1).toString().c_str());
+  // Wifi AP Part
+  Serial.printf("Ap Info\nEsp AP IPAddress: %s \nDHCP Settings\nGateway: %s \nSubnet: %s\nDNS: %s - %s\n",
+  WiFi.softAPIP().toString().c_str(),
+  SOFT_AP_Gateway.toString().c_str(),
+  SOFT_AP_Subnet.toString().c_str(),
+  SOFT_AP_DNS1.toString().c_str(),SOFT_AP_DNS2.toString().c_str());
+
+#endif
+
+#if NAT_Enabled  && !LWIP_IPV6
+  err_t nat_err = ip_napt_init(NAPT, NAPT_PORT);
+#if SERIAL_DEBUG
+  Serial.println("Initializing NAT");
+#endif
+  if (nat_err == ERR_OK)
+  {
+    nat_err = ip_napt_enable_no(SOFTAP_IF, 1);
+#if SERIAL_DEBUG
+    Serial.println("Enabling NAT on AP");
+#endif
+    if (nat_err == ERR_OK)
+    {
+#if SERIAL_DEBUG
+      Serial.printf("Nat Is Enabled on '%s' \n", SOFT_AP_SSID);
+#endif
+    }
+  }
+
+  if (nat_err != ERR_OK)
+  {
+#if SERIAL_DEBUG
+    Serial.println("ERR: NAT is not initialized");
+#endif
+  }
+#elif NAT_Enabled
+    Serial.println("ERR: NAT is not initialized");
+    Serial.println("\n\n\nPlease select LwIP Variant: v2 Higher Bandwidth or v2 Lower Memory");
+#endif
 }
 
-void loop() {
+#else
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("\n\n\nPlease select LwIP Variant: v2 Higher Bandwidth or v2 Lower Memory");
+}
+#endif
+
+void loop()
+{
 }
